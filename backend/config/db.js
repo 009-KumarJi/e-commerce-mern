b52f54d975c1330f626fc35efa6677ff7a6a2f89
009-KumarJi <krishnakumar091@yahoo.com>
2023-10-20 21:05:04 +0530

    mongoose config + models creations for user, product, order.

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async (URI) => {
+    console.log(`Connecting to MongoDB...`);
+    try {
+        const conn = await mongoose.connect(URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import products from "./data/products.js";
+import connectDB from "./config/db.js";
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const port = process.env.PORT || 5000;
+
+connectDB(process.env.MONGO_URI); // Connect to MongoDB
 
 const app = express();
 
