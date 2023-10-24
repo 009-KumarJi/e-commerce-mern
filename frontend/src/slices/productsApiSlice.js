6b5ff715d70ba840c1fab04e3058afd25b396960
009-KumarJi <krishnakumar091@yahoo.com>
2023-10-25 04:01:11 +0530

    product fetching modified to redux!

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -7,7 +7,12 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             query: () => ({url: PRODUCTS_URL}),
             keepUnusedDataFor: 5, // Keep unused data for 5 secs. This is useful for when we switch between pages.
         }),
+        getProductDetails: builder.query({
+            query: (productID) => ({url: `${PRODUCTS_URL}/${productID}`}),
+            keepUnusedDataFor: 5,
+        })
     }),
 });
 
 export const {useGetProductsQuery} = productsApiSlice;
+export const {useGetProductDetailsQuery} = productsApiSlice;
